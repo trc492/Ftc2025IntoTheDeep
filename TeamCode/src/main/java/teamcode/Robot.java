6fd88de3fce6c3961e260ee8138a32dec4e5fa19
Titan Robotics Club <trc492@users.noreply.github.com>
2024-08-24 03:45:36 -0700

    Sync'd with FtcTemplate.

--- TeamCode/src/main/java/teamcode/Robot.java
+++ TeamCode/src/main/java/teamcode/Robot.java
@@ -46,7 +46,6 @@ import trclib.timer.TrcTimer;
 public class Robot
 {
     private static final String moduleName = Robot.class.getSimpleName();
-    private static final double STATUS_UPDATE_INTERVAL = 0.1;   // 100 msec
     // Global objects.
     public final FtcOpMode opMode;
     public final TrcDbgTrace globalTracer;
@@ -125,7 +124,7 @@ public class Robot
     @Override
     public String toString()
     {
-        return robotInfo != null? robotInfo.robotName: RobotParams.System.DEF_ROBOT_NAME;
+        return robotInfo != null? robotInfo.robotName: RobotParams.Robot.ROBOT_CODEBASE;
     }   //toString
 
     /**
@@ -255,11 +254,11 @@ public class Robot
      */
     public void updateStatus()
     {
-        if (TrcTimer.getCurrentTime() > nextStatusUpdateTime)
+        double currTime = TrcTimer.getCurrentTime();
+        if (currTime > nextStatusUpdateTime)
         {
             int lineNum = 2;
-
-            nextStatusUpdateTime += STATUS_UPDATE_INTERVAL;
+            nextStatusUpdateTime = currTime + RobotParams.Robot.DASHBOARD_UPDATE_INTERVAL;
             if (robotDrive != null)
             {
                 dashboard.displayPrintf(lineNum++, "DriveBase: Pose=%s", robotDrive.driveBase.getFieldPosition());
@@ -267,6 +266,9 @@ public class Robot
             //
             // Display other subsystem status here.
             //
+            if (RobotParams.Preferences.showSubsystems)
+            {
+            }
         }
     }   //updateStatus
 

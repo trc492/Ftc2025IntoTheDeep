0f9f21b943c37632707664aebc204f5e310fda40
Writerfrighter <109774580+Writerfrighter@users.noreply.github.com>
2024-09-28 19:19:54 -0700

    Update AuxClimber.java

--- TeamCode/src/main/java/teamcode/subsystems/AuxClimber.java
+++ TeamCode/src/main/java/teamcode/subsystems/AuxClimber.java
@@ -22,11 +22,10 @@
 
 package teamcode.subsystems;
 
-import org.firstinspires.ftc.robotcore.internal.camera.names.CameraNameImplBase;
-
 import teamcode.RobotParams;
 import trclib.motor.TrcMotor;
 import ftclib.motor.FtcMotorActuator;
+import trclib.robotcore.TrcEvent;
 
 /**
  * This class creates the AuxClimber subsystem which consists of a climber motor actuator.
@@ -47,9 +46,49 @@ class AuxClimber
                 .setLowerLimitSwitch(RobotParams.ClimberParams.LOWER_LIMIT_NAME, RobotParams.ClimberParams.LOWER_LIMIT_INVERTED)
                 .setPositionScaleAndOffset(RobotParams.ClimberParams.INCHES_PER_COUNT, RobotParams.ClimberParams.POS_OFFSET)
                 .setPositionPresets(RobotParams.ClimberParams.POS_PRESET_TOLERANCE, RobotParams.ClimberParams.posPresets);
-        climber = null;
+        climber = new FtcMotorActuator(climberParams).getMotor();
     }   //AuxClimber
 
+    /**
+     * This method sets the Climber position.
+     *
+     * @param owner specifies the owner ID to check if the caller has ownership of the subsystem, can be null if
+     *        caller is not claiming ownership.
+     * @param pos specifies the climber position.
+     * @param powerLimit specifies the maximum power limit the Climber is moving.
+     * @param completionEvent specifies the event to signal when completed, can be null if not provided.
+     */
+    public void setClimberPosition(String owner, double pos, double powerLimit, TrcEvent completionEvent)
+    {
+        climber.setPosition(owner, 0.0, pos, true, powerLimit, completionEvent, 0.0);
+    }   //setClimberPosition
+
+    /**
+     * This method sets the motor power to move the Climber.
+     *
+     * @param owner specifies the owner ID to check if the caller has ownership of the subsystem, can be null if
+     *        caller is not claiming ownership.
+     * @param power specifies the power applied to the motor.
+     */
+    public void setClimberPower(String owner, double power)
+    {
+        climber.setPower(owner, 0.0,power, 0.0, null);
+    }   //setClimberPower
+
+    /**
+     * This method sets the motor power limit to move the Climber with PID control.
+     *
+     * @param owner specifies the owner ID to check if the caller has ownership of the subsystem, can be null if
+     *        caller is not claiming ownership.
+     * @param power specifies the PID power limit.
+     * @param minPos specifies the lowest Climber position limit.
+     * @param maxPos specifies the upper Climber position limit.
+     */
+    public void setClimberPidPower(String owner, double power, double minPos, double maxPos)
+    {
+        climber.setPidPower(owner, power, minPos, maxPos, false);
+    }   //setClimberPidPower
+    
     /**
      * This method returns the created Climber motor.
      *

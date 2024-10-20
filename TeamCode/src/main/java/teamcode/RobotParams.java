7726bc8f14e3ea15bb424ba9517b82c9220e6c43
Writerfrighter <noahjfang@outlook.com>
2024-10-20 16:15:29 -0700

    Creating Pick Up Specimen Autotask

--- TeamCode/src/main/java/teamcode/RobotParams.java
+++ TeamCode/src/main/java/teamcode/RobotParams.java
@@ -493,6 +493,7 @@ public class RobotParams
         public static final double MIN_POS                      = POS_OFFSET;
         public static final double MAX_POS                      = 120.0;
         public static final double GROUND_PICKUP_POS            = MIN_POS;
+        public static final double SPECIMEN_PICKUP_POS          = 30; // TODO: NEEDS TUNING
         public static final double[] posPresets                 = {MIN_POS, 30.0, 60.0, 90.0, MAX_POS};
         public static final double POS_PRESET_TOLERANCE         = 10.0;
 
@@ -525,6 +526,7 @@ public class RobotParams
         public static final double MIN_POS                      = POS_OFFSET;
         public static final double MAX_POS                      = 35.0;
         public static final double GROUND_PICKUP_POS            = MIN_POS;
+        public static final double SPECIMEN_PICKUP_POS          = 30; // TODO: NEEDS TUNING
         public static final double[] posPresets                 = {MIN_POS, 20.0, 25.0, 30.0, 35.0};
         public static final double POS_PRESET_TOLERANCE         = 3.0;
 

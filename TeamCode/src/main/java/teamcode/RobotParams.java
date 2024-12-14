9e9cf5b34c3d5b1c562506476332c56b5792d112
Writerfrighter <noahjfang@outlook.com>
2024-12-14 15:59:47 -0800

    Tuning for Auto

--- TeamCode/src/main/java/teamcode/RobotParams.java
+++ TeamCode/src/main/java/teamcode/RobotParams.java
@@ -128,7 +128,7 @@ public class RobotParams
         // Robot start locations in inches.
         public static final double STARTPOS_X                       = 0.5 * Field.FULL_TILE_INCHES;
         public static final double STARTPOS_Y                       = Field.HALF_FIELD_INCHES - Robot.ROBOT_LENGTH/2.0;
-        public static final TrcPose2D STARTPOSE_RED_NET_ZONE        = new TrcPose2D(-STARTPOS_X, -STARTPOS_Y, 180.0);
+        public static final TrcPose2D STARTPOSE_RED_NET_ZONE        = new TrcPose2D(-STARTPOS_X, -STARTPOS_Y, 0.0);
         public static final TrcPose2D STARTPOSE_RED_OBSERVATION_ZONE= new TrcPose2D(STARTPOS_X, -STARTPOS_Y, 180.0);
 
         public static final double AUTO_PERIOD                      = 30.0; // 30 seconds auto period
@@ -141,7 +141,7 @@ public class RobotParams
         public static final double CHAMBER_MAX_SCORE_POS_X          = (CHAMBER_LENGTH / 2.0) - 3.0;
         // Score poses (Net zone side).
         public static final TrcPose2D RED_BASKET_SCORE_POSE         =
-            new TrcPose2D(-2.37 * Field.FULL_TILE_INCHES, -2.37 * Field.FULL_TILE_INCHES, 45.0);
+            new TrcPose2D(-2.38 * Field.FULL_TILE_INCHES, -2.38 * Field.FULL_TILE_INCHES, 45.0);
         public static final TrcPose2D RED_NET_CHAMBER_SCORE_POSE    =
             new TrcPose2D(-0.1 * Field.FULL_TILE_INCHES, -1.3545 * Field.FULL_TILE_INCHES, 180.0);
         // Score pose (Observation zone side).
@@ -149,7 +149,7 @@ public class RobotParams
             new TrcPose2D(0.1 * Field.FULL_TILE_INCHES, -1.3545 * Field.FULL_TILE_INCHES, 180.0);
         // Pickup pose (Net zone side).
         public static final TrcPose2D RED_NET_ZONE_SPIKEMARK_PICKUP =
-            new TrcPose2D(-1.7 * Field.FULL_TILE_INCHES, -1.74 * Field.FULL_TILE_INCHES, 0.0);
+            new TrcPose2D(-1.7 * Field.FULL_TILE_INCHES, -1.775 * Field.FULL_TILE_INCHES, 0.0);
         // Pickup pose (Observation zone side).
         public static final TrcPose2D RED_OBSERVATION_ZONE_PICKUP   =
             new TrcPose2D(2.0 * Field.FULL_TILE_INCHES, -2.05 * Field.FULL_TILE_INCHES, 180.0);
